507188762f6d4398cc9f76364d34573648ec9e55
Жан-Клод <jeanyao@ymail.com>
2023-05-09 20:58:27 +0300

    Handler process

--- src/Component/Routing/Router.php
+++ src/Component/Routing/Router.php
@@ -5,6 +5,7 @@ use Closure;
 use Laventure\Component\Routing\Contract\RouteDispatcherInterface;
 use Laventure\Component\Routing\Contract\RouterInterface;
 use Laventure\Component\Routing\Exception\RouteNotFoundException;
+use Laventure\Component\Routing\Route\RouteGroup;
 
 
 /**
@@ -70,6 +71,27 @@ class Router implements RouterInterface
 
 
     /**
+     * Controller namespace
+     *
+     * @var string
+    */
+    protected $namespace;
+
+
+
+
+    /**
+     * Current route group
+     *
+     * @var RouteGroup
+    */
+    protected $routeGroup;
+
+
+
+
+
+    /**
      * route patterns
      *
      * @var array
@@ -89,7 +111,7 @@ class Router implements RouterInterface
     {
         $this->factory    = new RouteFactory();
         $this->collection = new RouteCollection();
-        $this->parameter  = new RouteParameter();
+        $this->routeGroup = new RouteGroup();
         $this->dispatcher = $dispatcher ?: new RouteDispatcher();
     }
 
@@ -109,6 +131,35 @@ class Router implements RouterInterface
 
         return $this;
     }
+
+
+
+
+    /**
+     * Set controller namespace
+     *
+     * @param string $namespace
+     *
+     * @return $this
+    */
+    public function namespace(string $namespace): static
+    {
+        $this->namespace = $namespace;
+
+        return $this;
+    }
+
+
+
+
+    /**
+     * @return string
+    */
+    public function getNamespace(): string
+    {
+        return $this->namespace;
+    }
+
 
 
 
@@ -160,16 +211,7 @@ class Router implements RouterInterface
         $route->domain($this->domain);
         $route->wheres($this->patterns);
 
-        if (is_array($handler)) {
-            $route->controller($handler[0], $handler[1] ?? '__invoke');
-        } elseif ($handler instanceof Closure) {
-            $route->callback($handler);
-        } elseif (is_string($handler)) {
-            list($controller, $action) = explode('@', $handler, 2);
-            $route->controller($controller, $action);
-        }
-
-        return $route;
+        return $this->processHandler($route, $handler);
     }
 
 
@@ -347,5 +389,30 @@ class Router implements RouterInterface
     public function generate(string $name, array $parameters = [])
     {
          return $this->collection->getNamedRoute($name)?->generatePath($parameters);
+    }
+
+
+
+
+
+    /**
+     * @param Route $route
+     *
+     * @param mixed $handler
+     *
+     * @return Route
+    */
+    private function processHandler(Route $route, $handler): Route
+    {
+        if (is_array($handler)) {
+            $route->controller($handler[0], $handler[1] ?? '__invoke');
+        } elseif ($handler instanceof Closure) {
+            $route->callback($handler);
+        } elseif (is_string($handler)) {
+            list($controller, $action) = explode('@', $handler, 2);
+            $route->controller($controller, $action);
+        }
+
+        return $route;
     }
 }
